04c015c653dc2b0e8b23c96ad70ce96fef6c4ffc
Jakub Piskorski <piskor.piskorski@gmail.com>
2021-09-12 14:01:35 +0200

    Revert "fixfix"
    This reverts commit d72b35c3807f260ceab2bcf3f4a4500aa9ae2392.

--- TabApp/Controllers/PersonController.cs
+++ TabApp/Controllers/PersonController.cs
@@ -56,6 +56,7 @@ namespace TabApp.Controllers
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
+        [OverrideAuthorizationAttribute]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("ID,Name,Surname,Address,Email,PhoneNumber")] Person person)
         {
